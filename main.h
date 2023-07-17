cefd80350f1731f2aead7d3459a4e6ae65d2fa1e
oussama laamri <laamri2002@gmail.com>
2023-07-17 23:05:15 +0000

    i add a strlen fnct and main.h

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/ait.h>
+
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+
+#endif
