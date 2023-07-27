57ae1b9857729bd915cdea13c319b672798cfd27
ZohraAS <zohraaitsaid@gmail.com>
2023-07-27 16:11:45 -0700

    main modification

--- shell.c
+++ shell.c
@@ -1,34 +1,43 @@
 #include "shell.h"
 /**
  * main - entry point
- *
+ * @argc: arg count
+ * @argv: arg vector
  * Return: 0  if succ
  *
  */
-int main(void)
+int main(int argc, char **argv)
 {
-	char *input;
-	char **args;
-	int stat;
+	info_t infArray[] = { INFO_INIT };
+	int fileDesc = 2;
 
-	do {
-		if (isatty(STDIN_FILENO))
-			write(1, "ous&zoh$ ", 9); /*prompt*/
-		input = read_line();
-		if (input == NULL)
-		{
-			exit(EXIT_FAILURE);
-		}
-		args = break_line(input);
-		if (args == NULL)
-		{
-			perror("Error");
-			free(input);
-			exit(EXIT_FAILURE);
-		}
-		stat = exe_cmd(args);
-		memory(input, args);
-	} while (stat != -1); /*Continue until -1 : run until user type exit*/
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fileDesc)
+		: "r" (fileDesc));
 
-	return (0);
+	if (argc == 2)
+	{
+		fileDesc = open(argv[1], O_RDONLY);
+		if (fileDesc == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_puts(argv[0]);
+				_puts(": 0: Can't open ");
+				_puts(argv[1]);
+				_putchar('\n');
+				_putchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfileDesc = fileDesc;
+	}
+	pop_envList(infArray);
+	read_hist(infArray);
+	hsh(infArray, argv);
+	return (EXIT_SUCCESS);
 }
